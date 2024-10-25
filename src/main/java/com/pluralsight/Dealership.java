7c0c2f9518d965b9f5e944cc5691f3a1cd08c461
DevinW <devinswilson03@gmail.com>
2024-10-25 13:14:37 -0400

    Added to dealership file manager and vehicle class

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,4 +1,8 @@
 package com.pluralsight;
 
 public class Dealership {
+    private String name;
+    private String Address;
+    private String phone;
+
 }
--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,4 +1,35 @@
 package com.pluralsight;
 
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class DealershipFileManager {
+
+    private static final String FILE_NAME = "dealership.csv";
+    private static ArrayList<Vehicle>vehicle =new ArrayList<Vehicle>();c
+    public static void loadVehicleInventory(){
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME));
+            String line;
+            while ((line = reader.readLine()) != null) {
+
+                String[] dealershipParts = line.split("\\|");
+                if (dealershipParts.length == 3) {
+
+
+
+
+                }
+            }
+
+
+        } catch (Exception e) {
+            System.err.println("This file does not exist!");;
+        }
+
+    }
+
 }
