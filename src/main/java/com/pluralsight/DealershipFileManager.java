9dd88c051c78821240825fa68c1eb1f02e32931b
DevinW <devinswilson03@gmail.com>
2024-10-25 14:05:45 -0400

    finihsed vehicle class

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -2,34 +2,53 @@ package com.pluralsight;
 
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 
 public class DealershipFileManager {
 
     private static final String FILE_NAME = "dealership.csv";
-    private static ArrayList<Vehicle>vehicle =new ArrayList<Vehicle>();c
-    public static void loadVehicleInventory(){
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME));
-            String line;
-            while ((line = reader.readLine()) != null) {
+    private static ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>();
 
+    public static void loadVehicleInventory() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME))) {
+            String line;
+
+
+            if ((line = reader.readLine()) != null) {
                 String[] dealershipParts = line.split("\\|");
                 if (dealershipParts.length == 3) {
-
-
-
+                    String dealershipName = dealershipParts[0];
+                    String dealershipAddress = dealershipParts[1];
+                    String dealershipPhone = dealershipParts[2];
 
                 }
             }
 
+            while ((line = reader.readLine()) != null) {
+                String[] vehicleParts = line.split("\\|");
+                if (vehicleParts.length == 8) {
+                    int vin = Integer.parseInt(vehicleParts[0]);
+                    int year = Integer.parseInt(vehicleParts[1]);
+                    String make = vehicleParts[2];
+                    String model = vehicleParts[3];
+                    String vehicleType = vehicleParts[4];
+                    String color = vehicleParts[5];
+                    int odometer = Integer.parseInt(vehicleParts[6]);
+                    double price = Double.parseDouble(vehicleParts[7]);
+
+
+                    Vehicle vehicle = new Vehicle(vin, year, make, model, vehicleType, color, odometer, price);
+                    vehicles.add(vehicle);
+                }
+            }
 
         } catch (Exception e) {
-            System.err.println("This file does not exist!");;
+            System.err.println("This file does not exist!");
+            e.printStackTrace();
         }
-
+        System.out.println(vehicles);
     }
 
 }
+
